1ce6e541c894fd95c9a34c1e8006a6c184c17023
manhhavu <manhha@htoh.io>
2023-05-17 19:04:09 +0200

    POC for private network K8s

--- infrastructure/production/main.tf
+++ infrastructure/production/main.tf
@@ -23,36 +23,34 @@ provider "scaleway" {
   region = var.region
 }
 
-
-resource "scaleway_vpc_public_gateway_ip" "gw01" {
-}
-
-resource "scaleway_vpc_public_gateway_dhcp" "dhcp01" {
-  subnet             = "192.168.1.0/24"
-  push_default_route = true
-}
-
-resource "scaleway_vpc_public_gateway" "pg01" {
-  name            = "public-gateway"
-  type            = "VPC-GW-S"
-  ip_id           = scaleway_vpc_public_gateway_ip.gw01.id
-  bastion_enabled = true
-}
-
-resource "scaleway_vpc_private_network" "pn01" {
+resource "scaleway_vpc_private_network" "dmz" {
   name = "dmz"
 }
 
-resource "scaleway_vpc_gateway_network" "dmz" {
-  gateway_id         = scaleway_vpc_public_gateway.pg01.id
-  private_network_id = scaleway_vpc_private_network.pn01.id
-  dhcp_id            = scaleway_vpc_public_gateway_dhcp.dhcp01.id
-  cleanup_dhcp       = true
-  enable_masquerade  = true
+resource "scaleway_vpc_private_network" "app" {
+  name = "app"
+}
+
+resource "scaleway_k8s_cluster" "cluster" {
+  name                        = "exp-cluster"
+  version                     = "1.27.1"
+  cni                         = "cilium"
+  delete_additional_resources = false
+  private_network_id          = scaleway_vpc_private_network.app.id
+}
+
+
+resource "scaleway_k8s_pool" "pool_default" {
+  cluster_id = scaleway_k8s_cluster.cluster.id
+  name       = "default"
+  node_type  = "PLAY2-MICRO"
+  size       = 1
+
+  depends_on = [scaleway_instance_security_group.kapsule]
 }
 
 resource "scaleway_rdb_instance" "db" {
-  name           = "test-db"
+  name           = "exp-db"
   node_type      = "DB-DEV-S"
   engine         = "PostgreSQL-14"
   is_ha_cluster  = false
@@ -61,83 +59,27 @@ resource "scaleway_rdb_instance" "db" {
   password       = "veryS3cre7!"
 
   private_network {
-    pn_id = scaleway_vpc_private_network.pn01.id
+    pn_id = scaleway_vpc_private_network.app.id
   }
 }
 
-resource "scaleway_instance_server" "app" {
-  project_id = var.project_id
-  type       = "DEV1-S"
-  image      = "ubuntu_jammy"
-  name       = "app"
-
-  tags = ["app", "managed-by:terraform"]
-
-  security_group_id = scaleway_instance_security_group.www.id
+resource "scaleway_rdb_acl" "main" {
+  instance_id = scaleway_rdb_instance.db.id
+  acl_rules {
+    ip = "0.0.0.0/0"
+    description = "default"
+  }
 }
 
-resource "scaleway_instance_private_nic" "app_dmz" {
-  server_id          = scaleway_instance_server.app.id
-  private_network_id = scaleway_vpc_private_network.pn01.id
-}
-
-resource "scaleway_instance_security_group" "www" {
-  project_id              = var.project_id
+resource "scaleway_instance_security_group" "kapsule" {
+  name                    = "kubernetes ${split("/", scaleway_k8s_cluster.cluster.id)[1]}"
   inbound_default_policy  = "drop"
   outbound_default_policy = "accept"
-
+  stateful                = true
   inbound_rule {
     action   = "accept"
-    port     = "22"
-    ip_range = "0.0.0.0/0"
+    protocol = "UDP"
+    port     = "500"
   }
-
-  inbound_rule {
-    action = "accept"
-    port   = "80"
-  }
-
-  inbound_rule {
-    action = "accept"
-    port   = "443"
-  }
-}
-
-resource "scaleway_lb_ip" "main" {
-  zone = "fr-par-1"
-}
-
-resource "scaleway_lb" "base" {
-  ip_id  = scaleway_lb_ip.main.id
-  zone   = scaleway_lb_ip.main.zone
-  type   = "LB-S"
-
-  private_network {
-    private_network_id = scaleway_vpc_private_network.pn01.id
-    dhcp_config = true
-  }
-
-  depends_on = [scaleway_vpc_public_gateway.pg01]
-}
-
-resource "scaleway_lb_backend" "backend01" {
-  lb_id            = scaleway_lb.base.id
-  name             = "backend01"
-  forward_protocol = "http"
-  forward_port     = "80"
-
-  server_ips = [
-    "192.168.1.224"
-  ]
-
-  health_check_http {
-    uri = "/_/health"
-  }
-}
-
-resource "scaleway_lb_frontend" "frontend01" {
-  lb_id        = scaleway_lb.base.id
-  backend_id   = scaleway_lb_backend.backend01.id
-  name         = "frontend01"
-  inbound_port = "80"
+  depends_on = [scaleway_k8s_cluster.cluster]
 }
