226347f73d8cb750da53a8a06131b62118b2c6c8
manhhavu <manhha@htoh.io>
2023-06-10 21:56:21 +0200

    Install ingress controller

--- infrastructure/staging/main.tf
+++ infrastructure/staging/main.tf
@@ -319,7 +319,7 @@ resource "kubernetes_manifest" "external_secret_azuredns_config" {
     apiVersion = "external-secrets.io/v1beta1"
     kind       = "ExternalSecret"
     metadata = {
-      name = "azuredns-config"
+      name      = "azuredns-config"
       namespace = "cert-manager"
     }
     spec = {
@@ -375,5 +375,53 @@ resource "kubernetes_manifest" "cert_manager_cluster_issuer" {
         }]
       }
     }
+  }
+}
+
+// ingress-controller
+
+resource "scaleway_lb_ip" "ingress_ip" {
+  zone       = var.zone
+  project_id = var.project_id
+}
+
+resource "helm_release" "nginx_ingress" {
+  name      = "nginx-ingress"
+  namespace = "kube-system"
+
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+
+  set {
+    name  = "controller.service.loadBalancerIP"
+    value = scaleway_lb_ip.ingress_ip.ip_address
+  }
+
+  // enable proxy protocol to get client ip addr instead of loadbalancer one
+  set {
+    name  = "controller.config.use-proxy-protocol"
+    value = "true"
+  }
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/scw-loadbalancer-proxy-protocol-v2"
+    value = "true"
+  }
+
+  // indicates in which zone to create the loadbalancer
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/scw-loadbalancer-zone"
+    value = scaleway_lb_ip.ingress_ip.zone
+  }
+
+  // enable to avoid node forwarding
+  set {
+    name  = "controller.service.externalTrafficPolicy"
+    value = "Local"
+  }
+
+  // enable this annotation to use cert-manager
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/scw-loadbalancer-use-hostname"
+    value = "true"
   }
 }
