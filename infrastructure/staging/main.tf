ac95ccddb2aa5f3fc47aa12a2eba96164ae42e34
manhhavu <manhha@htoh.io>
2023-06-12 08:16:17 +0200

    Use separate config file for ingress

--- infrastructure/staging/main.tf
+++ infrastructure/staging/main.tf
@@ -392,37 +392,19 @@ resource "helm_release" "nginx_ingress" {
   chart      = "ingress-nginx"
   create_namespace = true
 
+  values = [
+    "${file("values-ingress.yaml")}"
+  ]
+
   set {
     name  = "controller.service.loadBalancerIP"
     value = scaleway_lb_ip.ingress_ip.ip_address
-  }
-
-  // enable proxy protocol to get client ip addr instead of loadbalancer one
-  set {
-    name  = "controller.config.use-proxy-protocol"
-    value = "true"
-  }
-  set {
-    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/scw-loadbalancer-proxy-protocol-v2"
-    value = "true"
   }
 
   // indicates in which zone to create the loadbalancer
   set {
     name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/scw-loadbalancer-zone"
     value = scaleway_lb_ip.ingress_ip.zone
-  }
-
-  // enable to avoid node forwarding
-  set {
-    name  = "controller.service.externalTrafficPolicy"
-    value = "Local"
-  }
-
-  // enable this annotation to use cert-manager
-  set {
-    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/scw-loadbalancer-use-hostname"
-    value = "true"
   }
 }
 
@@ -434,6 +416,6 @@ resource "helm_release" "open_telemetry_collector" {
   create_namespace = true
 
   values = [
-    "${file("otel-values.yaml")}"
+    "${file("values-opentelemetry.yaml")}"
   ]
 }
