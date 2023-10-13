7ab078c67365c9bc7c79d8e3cb16dd1f66350f22
manhhavu <manhha@htoh.io>
2023-10-13 22:06:11 +0200

    Link DNS to Ingress controller in Pulumi

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -3,6 +3,7 @@ import * as k8s from "@pulumi/kubernetes"
 import { ExternalSecretsComponent } from './external-secrets'
 import { CertManagerComponent } from './cert-manager'
 import { IngressControllerComponent } from './ingress-controller'
+import * as azure from "@pulumi/azure"
 
 const config = new pulumi.Config()
 config.requireSecret("ssmAccessKey")
@@ -16,9 +17,13 @@ const certManager = new CertManagerComponent("cert-manager", {}, {
     dependsOn: [externalSecrets]
 })
 
-const ingressController = new IngressControllerComponent("ingress-nginx", {}, {
+export const ingressController = new IngressControllerComponent("ingress-nginx", {}, {
     dependsOn: [externalSecrets]
 })
+
+// const zone = azure.dns.Zone.get("htoh.app", "/subscriptions/a130654f-11e9-4af9-a215-a62b7dfcfc22/resourceGroups/rg-dns/providers/Microsoft.Network/dnsZones/htoh.app")
+
+// export const nameServers = zone.nameServers
 
 function toBase64(data: string): string {
     return Buffer.from(data).toString('base64')
--- pulumi/ingress-controller.ts
+++ pulumi/ingress-controller.ts
@@ -1,23 +1,21 @@
 import * as k8s from "@pulumi/kubernetes"
 import * as pulumi from "@pulumi/pulumi"
-import * as nginx from "@pulumi/kubernetes-ingress-nginx";
+import * as azure from "@pulumi/azure"
 
 export class IngressControllerComponent extends pulumi.ComponentResource {
-    private namespace: k8s.core.v1.Namespace
-    private chart: k8s.helm.v3.Release
 
     constructor(name: string, args: {}, opts?: pulumi.ComponentResourceOptions) {
         super("htoh:index:IngressControllerComponent", name, args, opts);
 
-        this.namespace = new k8s.core.v1.Namespace(name, {
+        const namespace = new k8s.core.v1.Namespace(name, {
             metadata: {
                 name: name
             }
         })
 
-        this.chart = new k8s.helm.v3.Release(name, {
+        const chart = new k8s.helm.v3.Release(name, {
             chart: "ingress-nginx",
-            namespace: this.namespace.metadata.name,
+            namespace: namespace.metadata.name,
             values: {
                 controller: {
                     service: {
@@ -28,9 +26,25 @@ export class IngressControllerComponent extends pulumi.ComponentResource {
                     }
                 }
             },
-            repositoryOpts:{
+            repositoryOpts: {
                 repo: "https://kubernetes.github.io/ingress-nginx",
             },
+        })
+
+        const controllerService = chart.resourceNames['Service/v1']
+            .apply((services: string[]) => services.find(s => s.endsWith('controller'))!)
+
+        const service = k8s.core.v1.Service
+            .get("ingress-nginx-controller", controllerService, {})
+            .status.loadBalancer.ingress.apply(ingress => ingress[0].hostname)
+        const zone = azure.dns.Zone.get("htoh.app", "/subscriptions/a130654f-11e9-4af9-a215-a62b7dfcfc22/resourceGroups/rg-dns/providers/Microsoft.Network/dnsZones/htoh.app")
+
+        new azure.dns.CNameRecord("wildcard-dev", {
+            name: "*.dev",
+            zoneName: zone.name,
+            resourceGroupName: zone.resourceGroupName,
+            ttl: 300,
+            record: service,
         })
     }
 }
