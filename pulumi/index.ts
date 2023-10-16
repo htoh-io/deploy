bc3b47cf09a366b82c09ef9ed296a294be1c3666
manhhavu <manhha@htoh.io>
2023-10-16 09:29:15 +0200

    Clean up ingress-controller

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -5,25 +5,28 @@ import { CertManagerComponent } from './cert-manager'
 import { IngressControllerComponent } from './ingress-controller'
 import * as azure from "@pulumi/azure"
 
-const config = new pulumi.Config()
-config.requireSecret("ssmAccessKey")
+const config = new pulumi.Config("htoh")
 
 const externalSecrets = new ExternalSecretsComponent("external-secrets", {
-    accessKey: config.requireSecret("ssmAccessKey").apply(toBase64),
-    secretKey: config.requireSecret("ssmSecretKey").apply(toBase64),
+    accessKey: config.requireSecret("ssm-access-key").apply(toBase64),
+    secretKey: config.requireSecret("ssm-secret-key").apply(toBase64),
 });
 
 const certManager = new CertManagerComponent("cert-manager", {}, {
     dependsOn: [externalSecrets]
 })
 
-const ingressController = new IngressControllerComponent("ingress-nginx", {}, {
+const zone = azure.dns.Zone.get(
+    "htoh.app",
+    config.require("zone-htoh-app")
+)
+
+const ingressController = new IngressControllerComponent("ingress-nginx", {
+    ingressVersion: config.require("ingress-version"),
+    zone: zone
+}, {
     dependsOn: [externalSecrets]
 })
-
-// const zone = azure.dns.Zone.get("htoh.app", "/subscriptions/a130654f-11e9-4af9-a215-a62b7dfcfc22/resourceGroups/rg-dns/providers/Microsoft.Network/dnsZones/htoh.app")
-
-// export const nameServers = zone.nameServers
 
 function toBase64(data: string): string {
     return Buffer.from(data).toString('base64')
--- pulumi/ingress-controller.ts
+++ pulumi/ingress-controller.ts
@@ -3,25 +3,25 @@ import * as pulumi from "@pulumi/pulumi"
 import * as azure from "@pulumi/azure"
 
 export class IngressControllerComponent extends pulumi.ComponentResource {
-    private service: k8s.core.v1.Service
 
-    constructor(name: string, args: {}, opts?: pulumi.ComponentResourceOptions) {
+    constructor(name: string, args: {
+        ingressVersion: pulumi.Input<string>,
+        zone: azure.dns.Zone
+    }, opts?: pulumi.ComponentResourceOptions) {
         super("htoh:index:IngressControllerComponent", name, args, opts);
 
-        const ingressNginx = new k8s.kustomize.Directory("scalewayIngressNginx", {
-            directory: "https://github.com/kubernetes/ingress-nginx/tree/controller-v1.9.3/deploy/static/provider/scw"
+        const zone = args.zone
+
+        const ingressNginx = new k8s.kustomize.Directory("scaleway-ingress-nginx", {
+            directory: `https://github.com/kubernetes/ingress-nginx/tree/controller-v${args.ingressVersion}/deploy/static/provider/scw`
         })
 
-        this.service = k8s.core.v1.Service.get("ingressNginxService", "ingress-nginx/ingress-nginx-controller", {
+        const service = k8s.core.v1.Service.get("ingress-nginx-service", "ingress-nginx/ingress-nginx-controller", {
             dependsOn: [ingressNginx]
         })
 
-        const controllerIp = this.service.status.loadBalancer.ingress[0].ip
-        const zone = azure.dns.Zone.get(
-            "htoh.app",
-            "/subscriptions/a130654f-11e9-4af9-a215-a62b7dfcfc22/resourceGroups/rg-dns/providers/Microsoft.Network/dnsZones/htoh.app"
-        )
-
+        const controllerIp = service.status.loadBalancer.ingress[0].ip
+        
         new azure.dns.ARecord("wildcard-record", {
             name: "*.dev",
             zoneName: zone.name,
@@ -38,18 +38,6 @@ export class IngressControllerComponent extends pulumi.ComponentResource {
             records: [controllerIp],
         })
 
-        // ZeroSSL verification
-        new azure.dns.CNameRecord("zerossl-record", {
-            name: "_432C6D2A9CAB9F1C4003002249EA0A30.api.dev",
-            zoneName: zone.name,
-            resourceGroupName: zone.resourceGroupName,
-            ttl: 300,
-            record: "A27D0810F285D59468E304146312B2ED.471558341C9F67DB4002A5244029DDCE.bdba85485860870.comodoca.com",
-        })
-
-        // Now let's deploy two applications which are identical except for the
-        // names. We will later configure the ingress to direct traffic to them,
-        // one domain name per application instance.
         const appName = 'hello-k8s'
         const appSvc = new k8s.core.v1.Service(`${appName}-svc`, {
             metadata: { name: appName, },
@@ -82,7 +70,6 @@ export class IngressControllerComponent extends pulumi.ComponentResource {
             },
         });
 
-        // Next, expose the app using an Ingress.
         const appIngress = new k8s.networking.v1.Ingress(`${appName}-ingress`, {
             metadata: {
                 name: "hello-k8s-ingress",
